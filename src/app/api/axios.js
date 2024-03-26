ebc1c6a0ce7f8b6bfc0413d0bf1b62d7a9e57f38
Thaylor Santos <thaylormichi@gmail.com>
2024-03-26 10:15:59 -0300

    Update axios.js

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://robocontroleapi-app-202403260946.delightfulisland-79d19479.brazilsouth.azurecontainerapps.io/';
+const BASE_URL = 'https://robocontroleapi-app-202403260946.delightfulisland-79d19479.brazilsouth.azurecontainerapps.io/';
 
 export default axios.create({
     baseURL: BASE_URL,
